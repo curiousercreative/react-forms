22343bd1e120870714e2743b272e6f2e143337e4
Winston Hoy <winston@commonenergy.us>
2021-08-26 15:11:51 -0400

    support disabled, type and value props for SubmitButton

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -10,25 +10,34 @@ import exists from '../util/exists.js';
  * @function SubmitButton
  * @param {string} [className = '']
  * @param {jsx} children
+ * @param {boolean} [disabled]
  * @param {boolean} [isLoading] will derive from parent form state if not provided
  * @param {boolean} [isValid] will derive from parent form state if not provided
  * @param {function} [onClick] if not supplied, button will be type="submit"
+ * @param {string} [type]
+ * @param {string} [value]
  * @return {jsx} button[type=submit|button]
  */
-export default function SubmitButton ({ children, className = '', isLoading, isValid, onClick }) {
+export default function SubmitButton ({ children, className = '', disabled, isLoading, isValid, onClick, type, value }) {
   const { form } = useContext(FormContext);
-  isLoading = exists(isLoading) ? isLoading : form.state.isLoading;
-  isValid = exists(isValid) ? isValid : !form.props.validateAsYouGo || form.state.isValid;
+  if (!exists(disabled)) disabled = isLoading || !isValid;
+  if (!exists(isLoading)) isLoading = form.state.isLoading;
+  if (!exists(isValid)) isValid = !form.props.validateAsYouGo || form.state.isValid;
   let classes = className.split(' ').concat('form__submit');
   if (isLoading) classes.push('form__submit--is_loading');
+  if (!type) type = onClick ? 'button' : 'submit';
 
-  let props = {
+  const props = {
     className: classes.join(' '),
-    disabled: isLoading || !isValid,
-    type: onClick ? 'button' : 'submit',
+    disabled,
+    type,
   };
 
   if (typeof onClick === 'function') props.onClick = onClick;
+  if (exists(value)) {
+    if (typeof value !== 'string') console.warn(new Error(`SubmitButton expected a value prop with type string but received of type ${typeof value}. The value prop is relayed directly to a button HTML element and will be coerced into string`));
+    props.value = value;
+  }
 
   return <button {...props}>{children}</button>;
 }
